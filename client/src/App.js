7416b377d5215cd6903a4db246b40aa4e92c7951
FaroukDev <fa.elayache1@gmail.com>
2022-03-02 17:39:51 +0100

    adding material ui and styling app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Student from './components/showStudent/showStudent.js';
+import CreateStudent from './components/createStudent/createStudent.js'
 import './App.css';
+import makeStyles from './styles';
+
+
 
 function App() {
+  const classes = makeStyles();
   return (
     <div className="App">
-      <h1>Dashboard</h1>
+      <Container maxWidth="lg">
+        <AppBar className={classes.appbar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
+            Sudents Create and show
+          </Typography>
+        </AppBar>
+        <Grow in>
+          <Container >
+            <Grid container justifyContent="space-between" alignItems="center" >
+              <Grid item xs={12} sm={7}>
+                <AppBar className={classes.appBar} position="static" color="inherit">
+                  <Student/>
+                </AppBar>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+              <AppBar className={classes.appBar} position="static" color="inherit">
+                  <CreateStudent/>
+                </AppBar>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+
--- server/controllers/student.js
+++ server/controllers/student.js
@@ -4,11 +4,20 @@ export const getStudents =  async (req,res) => {
 
         res.status(200).json(allStudents);
     } catch (error) {
-        
+        res.status(404).json({ message: error.message})
     }
    
 }
 
-export const createStudent =   (req,res) => {
-    res.send('Router is working');
+export const createStudent =  async (req,res) => {
+    const student = req.body;
+
+    const newStudent = new student(student);
+
+    try {
+        await newStudent.save();
+        res.status(201).json(newStudent);
+    } catch (error) {
+        res.status(409).json( { message: error.message });
+    }
 }
