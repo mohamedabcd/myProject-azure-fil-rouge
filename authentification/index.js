72db5a67f328738f50b74771e00b313690bff8cb
FaroukDev <fa.elayache1@gmail.com>
2022-04-07 19:47:07 +0200

    build command added

--- authentification/index.js
+++ authentification/index.js
@@ -1,29 +1,25 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const Students = require('./routes/Students.js')
+const Students = require("./routes/Students.js");
 app.use(express.json());
 
-
 const db = require("./models/index.js");
-const cors = require('cors')
-
+const cors = require("cors");
 
 app.use(cors());
 
 //Routers
-const postRouter = require('./routes/Students.js');
+const postRouter = require("./routes/Students.js");
 app.use("/students", postRouter);
 
-const commentsRouter = require('./routes/Comments.js');
+const commentsRouter = require("./routes/Comments.js");
 app.use("/comments", commentsRouter);
 
-const usersRouter = require('./routes/Users.js');
-app.use("/auth", usersRouter)
+const usersRouter = require("./routes/Users.js");
+app.use("/auth", usersRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(5001, () => {
     console.log("Server running on port 5001");
-  })
-})
-
+  });
+});
